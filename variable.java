490998ed377149b0920a7707904db0902d69874b
gopalsingh130 <singhgopal5544@gmail.com>
2025-08-11 11:14:15 +0530

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,13 @@
+public class variable {
+    public static void main(String[] args){
+        int a=5;
+        int b=6;
+        int sc=9;
+        System.out.println(a+b);
+        //System.out.println(a);
+        //System.out.println(sc);
+
+
+
+    }
+}
