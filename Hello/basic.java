8f01c170ec40075eef70b9e20b6c6f2569d307c7
gopalsingh130 <singhgopal5544@gmail.com>
2025-08-11 10:29:47 +0530

    Add files via upload

---
+++ Hello/basic.java
@@ -0,0 +1,14 @@
+public class basic{
+    public static void main(String[]args){ 
+        System.out.println("hello world");
+        System.out.println("gopal");
+        System.out.println("fd");
+        System.out.println("java");
+    }
+}
+
+
+
+
+
+
