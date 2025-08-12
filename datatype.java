434b37e0b4e607062811202862e590f086b7b2ed
gopalsingh130 <singhgopal5544@gmail.com>
2025-08-12 09:21:24 +0530

    Add files via upload

---
+++ datatype.java
@@ -0,0 +1,10 @@
+public class datatype {
+    public static void main(String[]args){
+        String name = "surya";
+        int arr[]={1,2,3,4};
+        System.out.println(name);
+        System.out.println(arr[0]);
+        System.out.println(arr[2]);
+
+    }
+}
